d3dad94c18eaa8f59753e3ba5e7e5e1d2274e216
sylvainlecoy <sylvain.lecoy@gmail.com>
2013-05-07 18:53:22 +0200

    Work in progress...

--- lib/Drupal/doctrine/Mapping/SchemaDriver.php
+++ lib/Drupal/doctrine/Mapping/SchemaDriver.php
@@ -11,6 +11,8 @@ use Doctrine\Common\Persistence\Mapping\Driver\MappingDriver;
 use Doctrine\Common\Persistence\Mapping\ClassMetadata;
 use Doctrine\DBAL\Types\Type;
 
+use Inflect;
+
 /**
  * Reads the mapping meta-data from Schema API.
  *
@@ -75,7 +77,7 @@ class SchemaDriver implements MappingDriver {
           // Enhances the schema referenced through this foreign key with an
           // association meta-data bound to the schema referencing it.
           // The columns referenced eventually produces ToMany relationships.
-          $schemas[$reference['table']]['associations'][$name] = $reference['columns'];
+          $schemas[$reference['table']]['inversedBy'][$name] = array_flip($reference['columns']);
         }
       }
     }
@@ -119,39 +121,74 @@ class SchemaDriver implements MappingDriver {
         // What is expressed is: "This entity has a property that is a reference
         // to an instance of another entity". In other words, using a ManyToOne
         // is the way to map OneToOne foreign key associations.
-        list($self, $target) = $this->extractForeignKeys($reference);
-        $mapping = $this->fieldToArray($self, $schema['fields'][$self]);
+        $mapping = array();
+        $mapping['fieldName'] = $name;
         $mapping['targetEntity'] = $this->getTargetEntity($reference['table']);
-        $mapping['joinColumns'][] = $this->joinColumnToArray($reference['columns']);
+        $mapping['joinColumns'] = $this->joinColumnToArray($schema['name'], $reference['columns']);
         $mapping['fetch'] = \Doctrine\ORM\Mapping\ClassMetadata::FETCH_LAZY;
 
-        if (isset($schema['unique keys'])) {
-          foreach ($schema['unique keys'] as $name => $columns) {
-            if (in_array($self, $columns)) {
-              // If the schema contains a unique key on the reference being
-              // mapped, the relation is a OneToOne association.
-              $metadata->mapOneToOne($mapping);
-              break;
-            }
-          }
+        if ($this->isForeignKeyUnique($schema['name'], $name)) {
+          // If the schema contains a unique key on the reference being
+          // mapped, the association is a OneToOne relationship.
+          $metadata->mapOneToOne($mapping);
         }
         else {
-          // If the schema does not contains a unique key on the reference being
-          // mapped, the relation is then a ManyToOne association.
+          // If the schema does not contains a unique key on the reference
+          // being mapped, the association is then a ManyToOne relationship.
           $metadata->mapManyToOne($mapping);
         }
 
         // Properties must be declared only once.
-        // TODO: Refactor to loop over fields.
-        unset($schema['fields'][$name]);
+        $schema['fields'] = array_diff_key($schema['fields'], $reference['columns']);
       }
     }
 
-    foreach ($this->getSchemaReferences($className) as $name => $references) {
-      // A foreign key in an external schema can represent either a OneToMany
-      // relationship or a ManyToMany relationship (unidirectional).
-      $debug = TRUE;
-//       list($self, $target) = $this->extractForeignKeys($reference);
+    if (isset($schema['inversedBy'])) {
+      // A reference in schema can represent either a OneToMany relationship if
+      // the join schema is composed of a unique field referencing the foreign
+      // entity or a ManyToMany relationship if not.
+      foreach ($schema['inversedBy'] as $target => $reference) {
+        if ($this->hasTargetEntity($target)) {
+          // If the target referencing the entity through a foreign key
+          // constraint exists, the nature of the association can be either a
+          // OneToMany (bidirectional), or OneToMany (self-referencing).
+        }
+        else {
+          // If the target does not exists, the nature of the association can be
+          // a OneToMany (unidirectional) when a unique constraint is set, or a
+          // ManyToMany otherwise. When the target entity does not exists, it
+          // usually mean the relationship is done via a join table.
+          if ($target == 'users_roles') {
+          if (count($this->schemas[$target]['foreign keys']) != 2) {
+            // The physical data model (PDM) of ManyToMany and OneToMany
+            // (unidirectional) relationship is composed of two foreign keys.
+            // Throws an exception if the expected value is not met.
+            throw new \UnexpectedValueException(sprintf('Join table must have exactly 2 foreign keys: %s.', $target));
+          }
+
+          // Target becomes the join table in this if-branch.
+          list($self, $joinTable, $target, $targetTable) = $this->getAssociation($schema['name'], $target);
+
+          $mapping = array();
+          $mapping['joinTable'] = $this->joinTableToArray($self, $joinTable, $target);
+          $mapping['targetEntity'] = $this->getTargetEntity($targetTable);
+//           Inflect\Inflect::pluralize($string);
+
+          if ($this->isForeignKeyUnique($joinTable, $target)) {
+            // If the join table defines a unique key on the target entity, the
+            // association is a OneToMany (unidirectional) relationship.
+            $metadata->mapOneToMany($mapping);
+          }
+          else {
+            // If the join table does not defines a unique key on the target
+            // entity, the association is then a ManyToMany relationship.
+            $metadata->mapManyToMany($mapping);
+          }
+
+          $debug = true;
+          }
+        }
+      }
     }
 
     $metadata->setPrimaryTable($primaryTable);
@@ -169,30 +206,18 @@ class SchemaDriver implements MappingDriver {
 
   }
 
-  private function extractForeignKeys($reference) {
-    if (count($reference['columns']) > 1) {
-      // Mapping of several columns for a foreign key is not supported and
-      // the execution should result into a RuntimeException. However Drupal
-      // seems not using compound foreign keys and Doctrine does not
-      // recommends the use of surrogate key due to additional PHP code that
-      // is necessary to handle this kind of keys.
-
-      // TODO: Add support for surrogate foreign keys.
-      throw \UnexpectedValueException(sprintf('Unsupported surrogate foreign key to the schema: %s.', $reference['table']));
-    }
-    else {
-      return array(key($reference['columns']), current($reference['columns']));
-    }
-  }
-
   /**
-   * Parses the given Field as array.
+   * Parses the given Field name as array.
    *
+   * @param string $schema
+   *   The schema name.
    * @param string $name
-   * @param array $field
+   *   The field name.
    * @return array
    */
-  protected function fieldToArray($name, $field) {
+  protected function fieldToArray($schema, $name) {
+    $field = $this->schemas[$schema]['fields'][$name];
+
     $mapping = array(
       'fieldName' => $name,
       'type'      => $this->getFieldType($field['type'], isset($field['size']) ? $field['size'] : 'normal'),
@@ -213,18 +238,65 @@ class SchemaDriver implements MappingDriver {
   /**
    * Parses the given join column as array.
    *
-   * @param array $joinColumn
+   * @param string $schema
+   * @param array $columns
    * @return array
    */
-  protected function joinColumnToArray($joinColumn) {
+  protected function joinColumnToArray($schema, $columns) {
+    $joinColumns = array();
+
+    foreach ($columns as $name => $reference) {
+      $joinColumns[] = array(
+        'name' => $name,
+     /* 'unique' => TODO, */
+     /* 'nullable' => TODO, */
+     /* 'onDelete' => TODO, */
+     /* 'columnDefinition' => $this->fieldToArray($schema, $name), */
+        'referencedColumnName' => $reference
+      );
+    }
+
+    return $joinColumns;
+  }
+
+  /**
+   * Parses the given join table as array.
+   *
+   * @param string $name
+   * @return array
+   */
+  protected function joinTableToArray($self, $joinTable, $target) {
+    $self = $this->schemas[$joinTable]['foreign keys'][$self];
+    $target = $this->schemas[$joinTable]['foreign keys'][$target];
+
     return array(
-      'name' => key($joinColumn),
-   /* 'unique' => $joinColumn->unique, */
-   /* 'nullable' => $joinColumn->nullable, */
-   /* 'onDelete' => $joinColumn->onDelete, */
-   /* 'columnDefinition' => $joinColumn->columnDefinition, */
-      'referencedColumnName' => current($joinColumn),
+      'name' => $joinTable,
+      'schema' => $joinTable,
+      'joinColumns' => $this->joinColumnToArray($self['table'], $self['columns']),
+      'inverseJoinColumns' => $this->joinColumnToArray($target['table'], $target['columns'])
     );
+  }
+
+  /**
+   * Whether or not a foreign key is unique.
+   *
+   * @return boolean
+   */
+  protected function isForeignKeyUnique($schema, $key) {
+    if (isset($this->schemas[$schema]['unique keys'])) {
+      foreach ($this->schemas[$schema]['unique keys'] as $keys) {
+        // Checks the difference between the keys composing a foreign key and
+        // a unique constraint defined in the schema. An empty difference means
+        // the compound (or not) foreign key is unique.
+        $columns = $this->schemas[$schema]['foreign keys'][$key]['columns'];
+        $diff = array_diff(array_keys($columns), $keys);
+        if (empty($diff)) {
+          return TRUE;
+        }
+      }
+    }
+
+    return FALSE;
   }
 
   /**
@@ -251,53 +323,15 @@ class SchemaDriver implements MappingDriver {
   }
 
   /**
-   * Gets the schema referencing the entity.
+   * Whether or not a target entity exists for a table name.
    *
-   * This function helps finding the toMany relationships, where an external
-   * schema is referencing the entity.
+   * @param string $table
+   *   The name of the table.
    *
-   * @param string $class
-   *   The name of the entity class.
-   *
-   * @throws \UnexpectedValueException
-   *
-   * @return array
-   *   An array of 'foreign keys' definition keyed by schema name.
+   * @return boolean
    */
-  protected function getSchemaReferences($class) {
-    $references = array();
-
-    if (empty(static::$entityToTableMap[$class])) {
-      // If entity class has not been found in entity map, throws an unexpected
-      // value exception indicating the value does not match with the entity list.
-      throw new \UnexpectedValueException(sprintf('Unknown entity type: %s.', $class));
-    }
-
-    // The schema references building process depends on Schema API through
-    // this function, as the driver relies on hook_schema(). Using this
-    // feature adds a substantial performance hit to schema driver as more
-    // meta-data has to be loaded into memory than might actually be necessary.
-    // This may not be relevant to scenarios where caching of meta-data is in
-    // place, however hits hardly in scenarios where no caching is used.
-    foreach (drupal_get_schema() as $name => $schema) {
-      if (isset($schema['foreign keys'])) {
-        // Loops over the foreign keys definition to find an eventual relation
-        // with the corresponding schema of the entity. Because the name of the
-        // constraint does not follow any conventions, the driver cannot rely
-        // on the key, but is enforced to compare every keys with the schema.
-        foreach (array_values($schema['foreign keys']) as $reference) {
-          // Compares the current value with the entity schema.
-          if ($reference['table'] == static::$entityToTableMap[$class]) {
-            // When a schema containing a reference to the entity being
-            // checked, add its references with the name of the schema as key
-            // and foreign keys definition as value.
-            $references[$name] = $schema['foreign keys'];
-          }
-        }
-      }
-    }
-
-    return $references;
+  protected function hasTargetEntity($table) {
+    return in_array($table, array_values($this->entityToSchemaMap));
   }
 
   /**
@@ -314,19 +348,46 @@ class SchemaDriver implements MappingDriver {
    * @return string
    */
   protected function getTargetEntity($table) {
-    static $map = array();
-
-    if (empty($map)) {
-      $map = array_flip(static::$entityToTableMap);
-    }
+    $map = array_flip($this->entityToSchemaMap);
 
     if (empty($map[$table])) {
       // If table name has not been found in reversed entity map, throws an
       // unexpected value exception mirroring the exception in getSchema().
-      throw new \UnexpectedValueException(sprintf('Unknown table name: %s.', $table));
+      throw new \UnexpectedValueException(sprintf('Table is not mapped to entity: %s.', $table));
     }
 
     return $map[$table];
+  }
+
+  /**
+   * Gets the association for the current entity defined by a join table.
+   *
+   * @param string $self
+   *   Entity table.
+   * @param string $joinTable
+   *   Join table.
+   *
+   * @return array
+   *   An indexed array whose values are:
+   *   - string: name of the foreign key referencing the current entity.
+   *   - string: name of the join table.
+   *   - string: name of the foreign key referencing the target entity.
+   *   - string: name of the table defining the target entity.
+   */
+  protected function getAssociation($self, $joinTable) {
+    foreach ($this->schemas[$joinTable]['foreign keys'] as $name => $key) {
+      if ($key['table'] == $self) {
+        $self = $name;
+      }
+      else {
+        $target = $name;
+        $targetTable = $key['table'];
+      }
+    }
+
+    // Since the function is not called when the join table does not defines a
+    // relationship with the entity, we can safely assumes self and target are set.
+    return array($self, $joinTable, $target, $targetTable);
   }
 
   /**
@@ -384,7 +445,7 @@ class SchemaDriver implements MappingDriver {
     static $classes = array();
 
     if (empty($classes)) {
-      foreach (array_keys(static::$entityToTableMap) as $className) {
+      foreach (array_keys($this->entityToSchemaMap) as $className) {
         if (/* TODO put this back: class_exists($className) && */!$this->isTransient($className)) {
           $classes[] = $className;
         }
@@ -398,7 +459,7 @@ class SchemaDriver implements MappingDriver {
    * {@inheritDoc}
    */
   public function isTransient($className) {
-    if (isset(static::$entityToTableMap[$className])) {
+    if (isset($this->entityToSchemaMap[$className])) {
       return FALSE;
     }
 
